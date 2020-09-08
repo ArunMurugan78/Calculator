3c26bbce05af91edb0ede2f0ff8175c35b626040
Arun Murugan <arunmurugan200@gmail.com>
2020-09-08 10:32:18 +0530

    completed it

--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
+let expression = "";
+
+const inputElement = document.getElementById("input");
 
 
+const clickHandler = (e) => {
+  switch (e.target.value) {
+
+    case "clear":
+      expression = "";
+      break;
+
+    case "del":
+        expression = expression.slice(0, -1);
+        break;
+
+    case "evaluate":
+      expression = eval(expression).toString();
+      break;
+
+    default:
+      if (e.target.value) {
+        expression += e.target.value;
+      }
+  }
+
+  inputElement.value = expression;
+};
+
+
+document.getElementById("button-group").addEventListener("click", clickHandler);
